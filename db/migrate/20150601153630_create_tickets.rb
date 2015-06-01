1421951d085e70737770bb3b156f1a757a89d693
DamianZajac <damian.zajc@gmail.com>
2015-06-01 15:37:30 +0000

    add model ticket

---
+++ db/migrate/20150601153630_create_tickets.rb
@@ -0,0 +1,14 @@
+class CreateTickets < ActiveRecord::Migration
+  def change
+    create_table :tickets do |t|
+      t.string :title
+      t.text :description
+      t.text :pioroty
+      t.text :difficulty
+      t.text :status
+      t.references :project, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
