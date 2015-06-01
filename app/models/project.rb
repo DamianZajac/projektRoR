c1900cd8a41bbb56a73eaeb3ea5e4efff8fc2fa9
DamianZajac <damian.zajc@gmail.com>
2015-06-01 13:59:04 +0000

    Revert "dssd"
    This reverts commit 9fa169401d8a168f625363445517864fa6463bbf.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,3 @@
 class Project < ActiveRecord::Base
-  has_many :ticets
   validates :title, presence: true, length: { minimum: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :projects do
-    resource :ticets
-  end
+  resources :projects
   
   root 'welcome#index'
 
