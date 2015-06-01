7f244cf11fc1b64ca1ed26ffbded679d55edaaca
DamianZajac <damian.zajc@gmail.com>
2015-06-01 21:22:47 +0000

    Revert "add user"
    This reverts commit e4546bc7c19c3e8c0fec1d1757b996ddb40ca2b1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :users
   resources :projects do
     resources :tickets
   end
   
-  root 'home#index'
+  root 'welcome#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
